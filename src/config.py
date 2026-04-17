b2d5b9cfe1628be0dc2bb8fb9610fbbcbc1fd911
Daniel Alcala Alvarez <daniel.alvarez@alleninstitute.org>
2026-04-17 09:20:03 -0700

    Update default directory paths

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 # config.py
 
-RIG_DIR_DEFAULT       = r"C:\Data\Rig post 3-10-2026"
-WATERCAL_DIR_DEFAULT  = r"C:\Data\Water-cal-2026"
-SPOTCHECK_DIR_DEFAULT = r"C:\Data\SpotCheck"
+RIG_DIR_DEFAULT       = r"\\allen\aind\scratch\AindBehavior.db\AindVrForaging\Rig"
+WATERCAL_DIR_DEFAULT  = r"\\allen\aind\scratch\vr-foraging\water_calibration\automatic_water_calibration"
+SPOTCHECK_DIR_DEFAULT = r"\\allen\aind\scratch\vr-foraging\water_calibration\SpotCheck"
 
 OK_MARGIN = 10.0
 STRIKE_MARGIN = 15.0
